fef2a84adabe0e0328145e3b20a08452a904b0a2
david870110 <david19980110@gmail.com>
2024-08-30 18:49:21 +0800

    la_module_flushlogic_0830

--- LogicAnalyzer_0830.v
+++ LogicAnalyzer_0830.v
@@ -86,7 +86,7 @@ wire la_change;             // la signal changed
 wire trace_push;            // trace is push into FIFO
 wire fifo_full;
 wire [31:0] trace_packet;
-reg [7:0] fifo_count;       //  Jiin: parameterize the size of fifo_count with FIFO_depth use $clog2
+reg [7:0] fifo_count;       // Jiin: parameterize the size of fifo_count with FIFO_depth use $clog2
 reg [7:0] tx_count;         // count # of transfer
 reg la_hpri_req_o;
 
@@ -94,8 +94,8 @@ reg la_hpri_req_o;
 assign axi_awready  = axi_awvalid & axi_wvalid & enable_la;
 assign axi_wready   = axi_awvalid & axi_wvalid & enable_la;
 
-// axilite read - axi_rdata can be available when axi_arvalid (axi_araddr) is valid,
-//              - it is simply a combinational logic
+// axilite read - axi_rdata can be available when axi_arvalid (axi_araddr) is valid
+
 assign axi_rvalid   = axi_arvalid;
 assign axi_rdata    = (axi_araddr[11:2] == 10'h000) ? {8'b0, la_enable} :   
                       (axi_araddr[11:2] == 10'h001) ? {25'b0, h_thresh} :       // @'h4
@@ -106,7 +106,6 @@ assign axi_arready  = axi_arvalid;
 
 // Jiin : enable_la is the internal configuration register, we can not use enable_la to reset configuration register
 //   use axi_reset_n instead
-
 always @ ( posedge axi_clk or negedge axi_reset_n ) begin
     if( !axi_reset_n) begin
         la_enable   <= 24'h0;
@@ -126,10 +125,6 @@ end
 
 assign la_change = |(la_enable & ( up_la_data != r_la_data)); 
 
-// Jiin - remove la_hpri_req
-//  assign la_reset_n = enable_la & ~la_hpri_req_o;     // Jiin - la_hpri_req_o ?? why this term
-
-
 // fifo full handling
 //  - push null-packet all 0
 //  - flush FIFO 
@@ -139,14 +134,12 @@ assign la_change = |(la_enable & ( up_la_data != r_la_data));
 // - signal waveform recovery
 //   - when receive null-packet -> generate a cycle of 'x' signals
 //   - generate signal waveforms with non-null packet with rc 
-//   - 
-
-always @ ( posedge axi_clk or negedge enable_la) begin   // note: cc_la_enable to reset
+always @ ( posedge axi_clk or negedge enable_la) begin  // note: cc_la_enable to reset
     if( !enable_la ) begin
         rc_count  <= 8'h01;
         r_la_data <= 24'b0;
     end else begin
-        if( !la_change & rc_count != 8'hff)       // signal is not changed
+        if( !la_change & rc_count != 8'hff)             // signal is not changed
             rc_count <= rc_count + 1;
         else begin
             r_la_data <= up_la_data;
@@ -154,30 +147,31 @@ always @ ( posedge axi_clk or negedge enable_la) begin   // note: cc_la_enable t
         end
     end 
 end
+
+
 // trace_push : condition to push a trace into fifo
 // 1. signal changes - la_change
 // 2. rc_count reach 255
-// 3. fifo_full -> push null packet
+// 3. fifo_full -> push null packet (issue : use fifo_full - 1 or fifo_count == l_thread)
 
 // --- FIFO Instance -----
 la_fifo
 #( .WIDTH(32)
-    //.DEPTH(128)              // Jiin - use new fifo ip with parameter DEPTH
+    //.DEPTH(128)               // Jiin - use new fifo ip with parameter DEPTH
 )   fifo_inst
 (
     .axis_clk (axis_clk) ,
-    // .axi_reset_n (axi_reset_n),         // use enable_la - soft-reset
     .axi_reset_n (enable_la),
-    .w_vld (trace_push),          // fifo push
-    .w_rdy (trace_push_ok),          // w_rdy
+    .w_vld (trace_push),        // fifo push
+    .w_rdy (trace_push_ok),     // w_rdy
     .data_in (trace_packet),
     .r_rdy (m_tready),          // directly connect to axis r_ready
     .r_vld (m_tvalid),          // connect to axis r_valid
     .data_out (m_tdata)
 );
 
-// fifo pop & fifo push , m_tvalid is the fifo_empty , trace_push_ok is the fifo_full
-wire fifo_pop = m_tready & m_tvalid; // & (fifo_count > 0);
+// fifo pop & fifo push , m_tvalid is the fifo_empty , trace_push_ok is the fifo_full - JIANG
+wire fifo_pop = m_tready & m_tvalid;
 wire fifo_push = trace_push & trace_push_ok;
 
 //  FIFO depth counter - it is used to asserts la_hpri_req
@@ -185,12 +179,11 @@ wire fifo_push = trace_push & trace_push_ok;
 //  push condition
 //  1. signal change: la_change
 //  2. rc (repeat-count) reach maximum, i.e. rc = 8'hff
-//  3. fifo_full_push - fifo_full & push
-
-// r_block_push - block fifo push when fifo full
-//  from fifo_full -> la_hpri_req_o (change to 0)
-// at the end of the r_block_push, push a null-packet
-// from fifo_full -> la_hpri_req_o (change to 0)
+//  3. fifo_full_push - fifo_full & push (null_pakcet) - JIANG
+//      r_block_push - block fifo push when fifo full
+//      from fifo_full -> la_hpri_req_o (change to 1)
+//      at the end of the r_block_push, push a null-packet (use la_hpri_req_o and r_block_push) - JIANG
+//      from fifo_full -> la_hpri_req_o (change to 0)
 
 reg r_block_push; 
 wire fifo_full_push;
@@ -202,8 +195,8 @@ always @ ( posedge axi_clk or negedge enable_la )begin
         else if( la_hpri_req_o == 1'b0) r_block_push <= 0; // delay one cycle(from fifo_count < l_thresh) - JIANG
     end
 
-assign fifo_full_push = r_block_push;
-assign trace_push = (la_change | (rc_count == 8'hff)) & fifo_full_push;
+assign fifo_full_push = ((r_block_push == 1'b1) & (la_hpri_req_o == 1'b0)); // (fifo_full_push == 1'b0 & (la_hpri_req_o == 1'b1) the end of r_block_push
+assign trace_push = (la_change | (rc_count == 8'hff)) & (!r_block_push | fifo_full_push); 
 assign trace_packet = {32{!fifo_full_push}} & {rc_count, (r_la_data & la_enable)}; 
 
 assign la_hpri_req = la_hpri_req_o;
